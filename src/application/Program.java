a05666e521ab0ae54ce9ac5068f060d7726b8047
Mateus Begosso <mateusbegosso@gmail.com>
2023-11-24 12:43:44 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author Mateus
@@ -14,7 +16,10 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+                
     }
     
 }
