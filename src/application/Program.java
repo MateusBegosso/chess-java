4478cf99788af3c9adf7f440c163f29e2b432f1a
Mateus Begosso <mateusbegosso@gmail.com>
2023-12-18 14:34:50 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,28 +23,28 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-
-        while (true) {
+        
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
-
+                
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
-
+                
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+                
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
@@ -56,7 +56,9 @@ public class Program {
                 sc.nextLine();
             }
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
+        
     }
-
+    
 }
