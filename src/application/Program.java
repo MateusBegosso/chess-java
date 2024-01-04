72ba26535941e29a45783dc8756adb9abc6f93f9
Mateus Begosso <mateusbegosso@gmail.com>
2024-01-04 18:57:22 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,11 +23,11 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        
+
         while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
@@ -35,18 +35,24 @@ public class Program {
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
-                
+
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
-                
+
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                
+
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
@@ -58,7 +64,7 @@ public class Program {
         }
         UI.clearScreen();
         UI.printMatch(chessMatch, captured);
-        
+
     }
-    
+
 }
